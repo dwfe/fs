8163792961b0a2f7c56f8c9d867f0431b38700c2
z <alex@dwfe.ru>
2022-02-05 04:05:53 +0300

    .

--- src/copy-sync.ts
+++ src/copy-sync.ts
@@ -1,5 +1,6 @@
 import {copyFileSync, existsSync, mkdirSync, readdirSync} from 'fs';
-import {join} from 'path';
+import {logErr, logWarn} from '@do-while-for-each/log-node';
+import {basename, extname, join} from 'path';
 import {isDirectory} from './directory';
 
 export function copySync(
@@ -12,6 +13,10 @@ export function copySync(
   if (isDirectory(src)) {
     if (!existsSync(dst))
       mkdirSync(dst);
+    if (!isDirectory(dst)) {
+      err(`Can't copy src dir "${src}" to dst file "${dst}"`);
+      throw '';
+    }
     let fileNames = readdirSync(src);
     if (allowedToCopyFilter)
       fileNames = fileNames.filter(allowedToCopyFilter);
@@ -22,6 +27,26 @@ export function copySync(
         allowedToCopyFilter
       );
     });
-  } else
+  } else {
+    const dstExt = extname(dst);
+    if (extname(src) !== dstExt) {
+      if (!dstExt && !existsSync(dst)) {
+        warn(`I will assume that dst "${dst}" is a directory`);
+        mkdirSync(dst);
+      } else
+        warn(`Different extensions for src file "${src}" and dst file "${dst}"`);
+    }
+    if (existsSync(dst) && isDirectory(dst))
+      dst = join(dst, basename(src));
     copyFileSync(src, dst);
+  }
 }
+
+function err(message: string): void {
+  logErr('Copy sync:', message);
+}
+
+function warn(message: string): void {
+  logWarn('Copy sync:', message);
+}
+
