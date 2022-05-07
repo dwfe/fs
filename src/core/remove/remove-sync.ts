86e8c926e55e3218884b781b30e75b228a3e6edb
z <alex@dwfe.ru>
2022-05-07 23:10:18 +0300

    .

--- src/core/remove/remove-sync.ts
+++ src/core/remove/remove-sync.ts
@@ -2,7 +2,7 @@ import {rmSync, unlinkSync} from 'fs';
 import {getStats, isDirectory, isSymbolicLink} from '../common';
 import {IRemoveOpt} from '../contract';
 
-export function removeSync(path: string, {force, stats}: IRemoveOpt): void {
+export function removeSync(path: string, {force, stats}: IRemoveOpt = {}): void {
   stats = stats || getStats(path);
   if (isSymbolicLink(path, stats))
     unlinkSync(path);
